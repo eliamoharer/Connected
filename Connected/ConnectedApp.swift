0c1f703feb6f4bd9718b2e54d06280496581ab76
Elia Moharer <elia.moharer@gmail.com>
2026-05-10 19:59:12 -0400

    Initial Commit

---
+++ Connected/ConnectedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectedApp.swift
+//  Connected
+//
+//  Created by Elia Moharer on 2026-05-10.
+//
+
+import SwiftUI
+
+@main
+struct ConnectedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
