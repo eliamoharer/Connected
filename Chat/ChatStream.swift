4265229b24685d0068e20b2a33caff168e6b0695
Elia Moharer <elia.moharer@gmail.com>
2026-06-18 02:23:46 -0400

    fix scrolling and add text animation to signal drawer opening

--- Chat/ChatStream.swift
+++ Chat/ChatStream.swift
@@ -89,3 +89,5 @@ extension ChatStream: StreamedMarkdownSource {
         }
     }
 }
+
+
--- ContentView.swift
+++ ContentView.swift
@@ -10,30 +10,45 @@ import SwiftUI
 struct ContentView: View {
     @StateObject private var vm = ChatViewModel()
     @StateObject private var fvm = FolderViewModel()
-        
+    
     @AppStorage("validToS") private var validToS = false
     
     @State private var drawerWidth: CGFloat = UIScreen.main.bounds.width
     
     var body: some View {
         if validToS {
-                ZStack(alignment: .leading) {
-                    Color("BackgroundColor")
-                        .ignoresSafeArea()
-                    
-                    VStack {
+            ZStack(alignment: .leading) {
+                Color("BackgroundColor")
+                    .ignoresSafeArea()
+                
+                VStack {
+                    ScrollViewReader { proxy in
                         ScrollView {
                             LazyVStack() {
                                 ForEach(vm.messages) {
-                                    currentMessage in ChatBubble(message: currentMessage, vm: vm, isEditing: vm.editingMessageID == currentMessage.id, isLast: ((vm.messages.last?.id == currentMessage.id) && !vm.model.isEmpty))
+                                    currentMessage in
+                                    ChatBubble(message: currentMessage,
+                                               vm: vm,
+                                               isEditing: vm.editingMessageID == currentMessage.id,
+                                               isLast: ((vm.messages.last?.id == currentMessage.id) && !vm.model.isEmpty))
+                                    .id(currentMessage.id)
                                 }
                             }
+                            .scrollTargetLayout()
                             //.animation(.bouncy, value: vm.messages.count)
                         }
                         .ignoresSafeArea(edges: .top)
                         .defaultScrollAnchor(.bottom, for: .sizeChanges)
                         .contentMargins(.top, 90, for: .scrollContent)
-                        .scrollDismissesKeyboard(.interactively)
+                        // interactively was breaking stuff
+                        .scrollDismissesKeyboard(.immediately)
+                        .onChange(of: vm.messages.count) {
+                            if let lastMessageID = vm.messages.last?.id {
+                                withAnimation(.snappy) {
+                                    proxy.scrollTo(lastMessageID, anchor: .bottom)
+                                }
+                            }
+                        }
                         
                         .overlay(alignment: .top) {
                             LinearGradient(
@@ -63,6 +78,27 @@ struct ContentView: View {
                                         .font(.caption)
                                         .italic()
                                         .opacity(0.5)
+                                        .keyframeAnimator(initialValue: 0.0, repeating: true) { content, xOffset in
+                                            content.offset(x: xOffset)
+                                        } keyframes: { _ in
+                                            KeyframeTrack {
+                                                // 1. Rest at the center
+                                                LinearKeyframe(0, duration: 0.5)
+                                                
+                                                CubicKeyframe(-drawerWidth/2 + 40, duration: 0.4)
+                                                LinearKeyframe(-drawerWidth/2 + 40, duration: 0.1)
+                                                
+                                                CubicKeyframe(-drawerWidth/2 + 65, duration: 0.5)
+                                                CubicKeyframe(-drawerWidth/2 + 55, duration: 0.3)
+                                                CubicKeyframe(-drawerWidth/2 + 85, duration: 0.5)
+                                                CubicKeyframe(-drawerWidth/2 + 75, duration: 0.3)
+                                                
+                                                CubicKeyframe(-drawerWidth/2 + 100, duration: 0.8, endVelocity: 0)
+                                                
+                                                SpringKeyframe(0, duration: 0.7, spring: .bouncy)
+                                            }
+                                        }
+                                    
                                     Spacer()
                                 }
                                 .font(.system(size: 17, design: .serif))
@@ -77,25 +113,26 @@ struct ContentView: View {
                         }
                         .ignoresSafeArea(.container, edges: .bottom)
                     }
-                    .allowsHitTesting(!fvm.isFolderOpen)
-                    .ignoresSafeArea(.keyboard, edges: fvm.isFolderOpen ? .bottom : [])
-                    
-                    FolderView(vm: vm, fvm: fvm)
-                        .offset(x: fvm.drawerPosition ?? -drawerWidth)
-                        .highPriorityGesture(fvm.FolderGesture(drawerWidth: drawerWidth))
-                        .allowsHitTesting(fvm.isFolderOpen)
-                    //.ignoresSafeArea()
                 }
-                .sensoryFeedback(.impact(weight: .light), trigger: fvm.isFolderOpen)
-                .simultaneousGesture(fvm.FolderGesture(drawerWidth: drawerWidth))
-                .onGeometryChange(for: CGFloat.self) { proxy in
-                    proxy.size.width + proxy.safeAreaInsets.leading + proxy.safeAreaInsets.trailing
-                } action: { fullWidth in
-                    drawerWidth = fullWidth
-                    if !fvm.isDragging {
-                        fvm.drawerPosition = fvm.isFolderOpen ? 0 : -fullWidth
-                    }
+                .allowsHitTesting(!fvm.isFolderOpen)
+                .ignoresSafeArea(.keyboard, edges: fvm.isFolderOpen ? .bottom : [])
+                
+                FolderView(vm: vm, fvm: fvm)
+                    .offset(x: fvm.drawerPosition ?? -drawerWidth)
+                    .highPriorityGesture(fvm.FolderGesture(drawerWidth: drawerWidth))
+                    .allowsHitTesting(fvm.isFolderOpen)
+                //.ignoresSafeArea()
+            }
+            .sensoryFeedback(.impact(weight: .light), trigger: fvm.isFolderOpen)
+            .simultaneousGesture(fvm.FolderGesture(drawerWidth: drawerWidth))
+            .onGeometryChange(for: CGFloat.self) { proxy in
+                proxy.size.width + proxy.safeAreaInsets.leading + proxy.safeAreaInsets.trailing
+            } action: { fullWidth in
+                drawerWidth = fullWidth
+                if !fvm.isDragging {
+                    fvm.drawerPosition = fvm.isFolderOpen ? 0 : -fullWidth
                 }
+            }
             
         }  else {
             ToSView()
@@ -107,3 +144,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
